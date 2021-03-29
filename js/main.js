5cedcc1a6ff087a6f9fe787d161d405b04810497
Chris Williamson <chriswill0629@gmail.com>
2021-03-29 14:45:04 -0400

    added a simple,  fun enhance to the search button  ;-)

---
+++ js/main.js
@@ -0,0 +1,16 @@
+            const btn = document.querySelector("button");
+            
+            const txt = document.querySelector('p');
+            
+
+            btn.addEventListener('click', updateBtn);
+
+            function updateBtn() {
+                if (btn.textContent == "Google Search") {
+                    btn.textContent = "ODIN SEARCH";
+                    txt.textContent = "I'm sorry, Dave, I'm afraid I can't do that.";
+                } else {
+                    btn.textContent = "Google Search";
+                    txt.textContent = " ";
+                }
+            }
